70d18e44c54ec5c6363627c23d6ac0cea6ec13d0
Nikita Zuev <nikita_zuev_98@mail.ru>
2019-06-11 01:31:56 +0800

    Выбор цветов + события при клике

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
   <div class="game">
     <div class="game__inner">
       <div class="game__grid">
-        <!-- Сдаюсь, не могу понять, как мне отправить обьект Vue и приделать к нему класс.. -->
         <div
-          class="game__grid-item"  
-          :class="{ active: isActive }"
-          v-for="(item, index) in grid"
-          :key="index"
-          @click="selectElem(item, index)"
+          class="game__grid-item"
+          v-for=" item in grid"
+          :key="item"
+          @click="selectElem($event)"
         ></div>
       </div>
       <button class="game__button">Старт</button>
@@ -22,28 +20,99 @@ export default {
   name: "app",
   data() {
     return {
-      isActive: false,
+      number: 24,
+      colors: [],
+      first: false,
+      second: false,
+      animation: false,
       grid: []
     };
   },
+
   created() {
-    this.creatgrid();
+		this.creatgrid();
+    this.notRandomColors();
   },
+
   methods: {
     creatgrid() {
-      for (let i = 0; i < 16; i++) {
-        this.grid[i] = false;
-      }
+      for (let i = 0; i < this.number; i++) {
+        this.grid[i] = i;
+			}
+			console.log(this.grid);
     },
-    selectElem(item, index) {
-      console.log(item, index);
-      console.log(this);
+    notRandomColors() {
+      let colorsCol = this.number / 2,
+        radix = 0,
+        colorsTemplate = [];
+
+      while (radix * radix * radix < colorsCol) {
+        radix++;
+      }
+
+      createColorsTemplate();
+			createColors(this.colors);
+			randomColors(this.colors);
+			fillGrid(this.colors, this.grid);
+
+      function createColorsTemplate() {
+        for (let i = 0; i < colorsCol; i++) {
+          colorsTemplate[i] = "";
+          let one = Math.floor(i / (radix * radix)),
+            two = Math.floor((i - one * radix * radix) / radix),
+            three = i - one * radix * radix - two * radix;
+          colorsTemplate[i] += one;
+          colorsTemplate[i] += two;
+          colorsTemplate[i] += three;
+        }
+      }
+
+      function createColors(colors) {
+        for (let i = 0; i < colorsCol; i++) {
+          colors[i] = [];
+          for (let j = 0; j < 3; j++) {
+            colors[i][j] = Math.floor((255 / 2) * colorsTemplate[i][j]);
+          }
+        }
+      }
+
+      function randomColors(colors) {
+        colors = colors.concat(colors);
+        function compareRandom(a, b) {
+          return Math.random() - 0.5;
+        }
+        colors.sort(compareRandom);
+        colors.sort(compareRandom);
+      }
+
+      function fillGrid(colors, grid) {
+							
+			}
+    },
+
+    selectElem(event) {
+      if (!this.animation) {
+        if (!this.first) {
+          this.first = event.target;
+          this.first.classList.add("active");
+        } else {
+          this.second = event.target;
+          this.second.classList.add("active");
+          if (this.first) {
+            let that = this;
+            this.animation = true;
+            setTimeout(function() {
+              that.first.classList.remove("active");
+              that.second.classList.remove("active");
+              that.animation = false;
+            }, 1000);
+          }
+        }
+      }
     }
   }
-  // components: {}
 };
 </script>
-
 
 <style scoped lang="scss">
 .game {
@@ -62,9 +131,29 @@ export default {
   }
   &__grid-item {
     box-sizing: border-box;
+    position: relative;
     width: 25%;
     height: 100px;
     border: 1px solid #0998b8;
+    background-color: #abbfd4;
+
+    &:after {
+      content: "";
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background-color: #fff;
+      left: 0;
+      top: 0;
+      transition: 0.5s;
+    }
+    &.active:after {
+      background-color: rgba(#fff, 0);
+    }
+    &.finished:after {
+      background-color: rgba(#fff, 0);
+      border: 1px sloid #ffc0cb;
+    }
   }
 }
 </style>
